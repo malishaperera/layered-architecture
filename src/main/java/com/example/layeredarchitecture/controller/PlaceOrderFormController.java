0e20a0420774c4f2706ca8d3031a993523a6248f
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2023-12-16 16:12:26 +0530

    update 11111

--- src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -226,6 +226,7 @@ public class PlaceOrderFormController {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 
     private void loadAllItemCodes() {
